890ace8196bcb573080ca4a87440aae9f629a7c0
JakeFC <jacobchavera@yahoo.com>
2021-06-22 20:52:49 +0000

    404 page

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ running flask """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -13,6 +13,12 @@ app.register_blueprint(app_views)
 def teardown(error):
     """ calling close"""
     storage.close()
+
+
+@app.errorhandler(404)
+def error_handler(error):
+    """ 404 error page redirection """
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == '__main__':
     app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
